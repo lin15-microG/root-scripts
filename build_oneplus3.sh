ef9d3b43a1659c60e9480035a77e9c89b330c808
MSe1969 <mse1969@posteo.de>
2018-07-29 10:50:39 +0200

    Adapt build script f. parameter passing
    Change-Id: Icb360dc914db9f60608e52a1255547d3a60fa19a

--- build_oneplus3.sh
+++ build_oneplus3.sh
@@ -5,7 +5,7 @@ export CCACHE_DIR=~/.ccache15
 export OUT_DIR_COMMON_BASE=~/out-android
 
 # Use pre-defined build script
-source z_patches/build_oneplus3.sh
+source z_patches/build_oneplus3.sh $1
 
 
 
