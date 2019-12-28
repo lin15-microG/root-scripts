7fce3f962600680a0f318d595d17bb6c5f3634cd
MSe1969 <mse1969@posteo.de>
2019-12-28 22:15:09 +0100

    Rework switch script f. Treble/GSI target
    Change-Id: Idbbb6ad4acd1cd333c7b04f59ba06b8ce4045ca7

--- switch_microG.sh
+++ switch_microG.sh
@@ -28,16 +28,33 @@ case "$1" in
   microG) 
     BRANCH1="lin-15.1-microG"
     BRANCH2="lin-15.1-microG"
+    BRANCH3="lineage-15.1"
+    BRANCH4="lin-15.1-microG"
+    BRANCH5="lin-15.1-microG"
     PATCHV="S"
     ;;
   default) 
     BRANCH1="lineage-15.1"
+    BRANCH2="lineage-15.1"
+    BRANCH3="lineage-15.1"
+    BRANCH4="lin-15.1-microG"
+    BRANCH5="lin-15.1-microG"
+    PATCHV="S"
+    ;;
+  treble) 
+    BRANCH1="lin-15.1-treble"
     BRANCH2="lin-15.1-microG"
+    BRANCH3="lin-15.1-treble"
+    BRANCH4="lin-15.1-treble"
+    BRANCH5="lin-15.1-microG"
     PATCHV="S"
     ;;
   reference) 
     BRANCH1="lineage-15.1"
     BRANCH2="lineage-15.1"
+    BRANCH3="lineage-15.1"
+    BRANCH4="lineage-15.1"
+    BRANCH5="lineage-15.1"
     PATCHV="N"
     ;;
   *) 
@@ -45,6 +62,7 @@ case "$1" in
     echo "-"
     echo "  default   - LineageOS 15.1 (with patches and fixed repos)"
     echo "  microG    - hardened microG build"
+    echo "  treble    - Treble GSI build"
     echo "  reference - 100% LineageOS 15.1 (no patches - for 'repo sync')"
     exit
     ;;   
@@ -52,23 +70,26 @@ esac
 
 switch_zpatch $BRANCH1 R
 
+switch_branches $BRANCH3 build/make
+switch_branches $BRANCH3 device/lineage/sepolicy
+switch_branches $BRANCH3 external/selinux
 switch_branches $BRANCH1 frameworks/base
 switch_branches $BRANCH1 frameworks/native
-switch_branches $BRANCH1 kernel/oneplus/msm8996/
-switch_branches $BRANCH1 packages/apps/Camera2
-switch_branches $BRANCH1 packages/apps/Dialer
-switch_branches $BRANCH2 packages/apps/Eleven
-switch_branches $BRANCH2 packages/apps/LockClock
-switch_branches $BRANCH1 packages/apps/Jelly
-switch_branches $BRANCH1 packages/apps/Settings
-switch_branches $BRANCH1 packages/apps/SetupWizard
-switch_branches $BRANCH1 packages/apps/Trebuchet
-switch_branches $BRANCH2 system/bt
+switch_branches $BRANCH2 kernel/oneplus/msm8996/
+switch_branches $BRANCH2 packages/apps/Camera2
+switch_branches $BRANCH2 packages/apps/Dialer
+switch_branches $BRANCH5 packages/apps/Eleven
+switch_branches $BRANCH5 packages/apps/LockClock
+switch_branches $BRANCH2 packages/apps/Jelly
+switch_branches $BRANCH2 packages/apps/Settings
+switch_branches $BRANCH2 packages/apps/SetupWizard
+switch_branches $BRANCH2 packages/apps/Trebuchet
+switch_branches $BRANCH4 system/bt
 switch_branches $BRANCH1 system/core
-switch_branches $BRANCH1 system/sepolicy
-switch_branches $BRANCH1 vendor/lineage
+switch_branches $BRANCH2 system/sepolicy
+switch_branches $BRANCH2 vendor/lineage
 switch_branches $BRANCH1 .repo/local_manifests
-switch_branches $BRANCH1 OTA
+switch_branches $BRANCH2 OTA
 
 switch_zpatch $BRANCH1 $PATCHV
 
